7f5dc453a5c34ca53e9c85bf23cbfd084c489a6e
Ryan <ryan.gendel@bluelabellabs.com>
2023-04-25 14:19:39 -0600

    added env variable capabiities

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
 
+
 app.get('/', function (req, res) {
+console.log(process.env.S3_BUCKET)
+console.log(process.env.WHOAMI)
   res.send('Hello World')
 })
 
