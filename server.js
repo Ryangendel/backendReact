8d1aa4f1b7951d3c4d4ff5352307c0658885e4c2
Ryan <ryan.gendel@bluelabellabs.com>
2023-04-25 14:29:17 -0600

    added some data

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ console.log(process.env.WHOAMI)
   res.send('Hello World')
 })
 
+app.get('/getdata', function (req, res) {
+    console.log(process.env.S3_BUCKET)
+    console.log(process.env.WHOAMI)
+      res.json({name:"Runa", age:4, breed:"pit"})
+    })
+
 app.listen(PORT,()=>{
     console.log("LISTENING ON PORT "+PORT)
 })
